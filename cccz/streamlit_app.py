3ee996c606abcc43eaa281006d59f6fe5afe24fd
Jakub Schimer <jakub.schimer@protonmail.com>
2023-06-09 16:01:11 +0200

    w

--- cccz/streamlit_app.py
+++ cccz/streamlit_app.py
@@ -3,11 +3,13 @@ import os
 import pandas as pd
 import streamlit as st
 
-raw_data = {}
 
-
-@st.cache
+@st.cache_data
 def load_data():
+    rd = {}
+    locs = []
+    yrs = []
+
     data_file_paths = []
     for root, dir_, files in os.walk('./data'):
         for filename in files:
@@ -17,26 +19,91 @@ def load_data():
 
     for fp_year in data_file_paths:
         with open(fp_year[0], encoding='utf-8') as f:
+            yrs.append(fp_year[1])
             lines = f.readlines()[1:]
-            locations = {}
+            locations_ = {}
             for line in lines:
                 line_arr = line.split(',')
-                locations[line_arr[0]] = {
+                if line_arr[0] not in locs:
+                    locs.append(line_arr[0])
+
+                locations_[line_arr[0]] = {
                     'month_temperatures': [
                         float(x) for x in line_arr[1:-1]
                     ],
                     'year_temperature': float(line_arr[-1].strip())
                 }
 
-            raw_data[fp_year[1]] = locations
+            rd[int(fp_year[1])] = locations_
+
+    yrs = sorted(yrs)
+
+    return rd, locs, yrs
 
 
-df = pd.DataFrame(
-    {
-        'temperatures': raw_data[2022]['month_temperatures'],
-        'months': range(1, 13)
-    },
-    columns=['temperatures', 'months']
-)
+raw_data, locations, years = load_data()
 
-st.line_chart(df)
+selected_location = st.selectbox("Location", locations)
+selected_year = st.selectbox("Year", years)
+selected_month = st.selectbox("Month", range(1, 13))
+t1, t2, t3 = st.tabs([
+    "Year Temperature by  Month",
+    "Temperature by Year",
+    "Month temperature by Years"
+])
+# ==============================================================================
+
+with t1:
+    st.caption("Temperatures by Month")
+    df = pd.DataFrame(
+        {
+            'temperatures': raw_data[int(selected_year)][selected_location][
+                'month_temperatures'
+            ],
+            'months': range(1, 13)
+        },
+        columns=['temperatures', 'months'],
+    )
+    st.line_chart(df, x='months')
+
+# ==============================================================================
+
+yr_temps = []
+yrs = []
+for k, v in raw_data.items():
+    if selected_location in raw_data[k].keys():
+        t = raw_data[k][selected_location]['year_temperature']
+        yr_temps.append(t)
+        yrs.append(k)
+
+with t2:
+    st.caption("Temperatures by Year")
+    df2 = pd.DataFrame(
+        {
+            'temperatures': yr_temps,
+            'years': yrs
+        },
+        columns=['temperatures', 'years'],
+    )
+    st.line_chart(df2, x='years')
+
+# ==============================================================================
+
+
+month_temps_by_yr = []
+for k, v in raw_data.items():
+    if selected_location in raw_data[k].keys():
+        t = raw_data[k][selected_location]['month_temperatures'][
+            selected_month - 1
+            ]
+        month_temps_by_yr.append(t)
+
+with t3:
+    df3 = pd.DataFrame(
+        {
+            'temperatures': month_temps_by_yr,
+            'years': yrs
+        },
+        columns=['temperatures', 'years'],
+    )
+    st.line_chart(df3, x='years')
